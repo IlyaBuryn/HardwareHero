a1cc55d12a9ced7663c8d24e89ffec7508a7293d
IlyaBuryn <ilya.buryn@gmail.com>
2023-05-02 19:25:41 +0300

    Added API and business logic for `Mail` microservice.

---
+++ Services/HardwareHero.Services.Shared/DTOs/Mail/MailMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HardwareHero.Services.Shared.DTOs.Mail
+{
+    public class MailMessageDto
+    {
+        public Guid Id { get; set; }
+        public string MessageTitle { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Guid SenderId { get; set; }
+    }
+}
---
+++ Services/HardwareHero.Services.Shared/Models/Mail/MailMessage.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace HardwareHero.Services.Shared.Models.Mail
+{
+    public class MailMessage
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string MessageTitle { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Guid SenderId { get; set; }
+    }
+}

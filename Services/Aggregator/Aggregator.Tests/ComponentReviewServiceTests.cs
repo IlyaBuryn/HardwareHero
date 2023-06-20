a04b21f574366af97755893c3d456729e59507e1
IlyaBuryn <ilya.buryn@gmail.com>
2023-06-20 23:45:31 +0300

    Added tests.

--- Services/Aggregator/Aggregator.Tests/ComponentReviewServiceTests.cs
+++ Services/Aggregator/Aggregator.Tests/ComponentReviewServiceTests.cs
@@ -1,4 +1,16 @@
 ﻿using Aggregator.BusinessLogic.Contracts;
+<<<<<<< HEAD
+using Aggregator.BusinessLogic.Services;
+using AutoMapper;
+using FluentAssertions;
+using HardwareHero.Services.Shared.DTOs;
+using HardwareHero.Services.Shared.Exceptions;
+using HardwareHero.Services.Shared.Models.Aggregator;
+using HardwareHero.Services.Shared.Repositories.Contracts;
+using Moq;
+using System.Linq.Expressions;
+=======
+>>>>>>> 6b38c6b318f9e8ea2478d3e914f7535c735db017
 
 namespace Aggregator.Tests
 {
@@ -8,11 +20,95 @@ namespace Aggregator.Tests
 
         public ComponentReviewServiceTests()
         {
+<<<<<<< HEAD
+=======
             // Инициализация сервиса отзывов о компонентах перед каждым тестом (если необходимо)
+>>>>>>> 6b38c6b318f9e8ea2478d3e914f7535c735db017
             
         }
 
         [Fact]
+<<<<<<< HEAD
+        public async Task AddComponentReviewAsync_ReturnNotNullValue()
+        {
+            // Arrange
+            var mockRepository = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentReviewService(mockRepository.Object, mockMapper.Object);
+            var componentReviewDto = new ComponentReviewDto();
+
+            // Act
+            var result = await service.AddComponentReviewAsync(componentReviewDto);
+
+            // Assert
+            result.Should().NotBe(Guid.Empty);
+        }
+
+        [Fact]
+        public async Task GetComponentReviewsAsPageByComponentIdAsync_ReturnNotEmptyList()
+        {
+            // Arrange
+            var mockRepository = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentReviewService(mockRepository.Object, mockMapper.Object);
+            var pageNumber = 1;
+            var pageSize = 10;
+            var componentId = Guid.NewGuid();
+            var reviews = new List<ComponentReview> { new ComponentReview() };
+
+            mockRepository.Setup(repo => repo.GetManyEntitiesAsync(It.IsAny<Expression<Func<ComponentReview, bool>>>()))
+                .ReturnsAsync(reviews.AsQueryable());
+            mockRepository.Setup(repo => repo.GetPageAsync(It.IsAny<IQueryable<ComponentReview>>(), pageNumber, pageSize))
+                .ReturnsAsync(reviews);
+
+            // Act
+            var result = await service.GetComponentReviewsAsPageByComponentIdAsync(pageNumber, pageSize, componentId);
+
+            // Assert
+            result.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task GetComponentReviewsAsPageByComponentIdAsync_ThrowPageOptionsException()
+        {
+            // Arrange
+            var mockRepository = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentReviewService(mockRepository.Object, mockMapper.Object);
+            var pageNumber = 0;
+            var pageSize = 0;
+            var componentId = Guid.NewGuid();
+
+            // Act & Assert
+            await Assert.ThrowsAsync<PageOptionsValidationException>(() =>
+                service.GetComponentReviewsAsPageByComponentIdAsync(pageNumber, pageSize, componentId));
+        }
+
+        [Fact]
+        public async Task GetComponentReviewsAsPageByComponentIdAsync_ReturnEmptyList()
+        {
+            // Arrange
+            var mockRepository = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentReviewService(mockRepository.Object, mockMapper.Object);
+            var pageNumber = 1;
+            var pageSize = 10;
+            var componentId = Guid.NewGuid();
+            var reviews = new List<ComponentReview>();
+
+            mockRepository.Setup(repo => repo.GetManyEntitiesAsync(It.IsAny<Expression<Func<ComponentReview, bool>>>()))
+                .ReturnsAsync(reviews.AsQueryable());
+
+            // Act
+            var result = await service.GetComponentReviewsAsPageByComponentIdAsync(pageNumber, pageSize, componentId);
+
+            // Assert
+            result.Should().BeEmpty();
+=======
         public void AddComponentReviewAsync_ReturnNotNullValue()
         {
             Task.Delay(35).Wait();
@@ -65,6 +161,7 @@ namespace Aggregator.Tests
             // Act
 
             // Assert
+>>>>>>> 6b38c6b318f9e8ea2478d3e914f7535c735db017
         }
     }
 }
--- Services/Aggregator/Aggregator.Tests/ComponentServiceTests.cs
+++ Services/Aggregator/Aggregator.Tests/ComponentServiceTests.cs
@@ -1,4 +1,16 @@
 using Aggregator.BusinessLogic.Contracts;
+<<<<<<< HEAD
+using Aggregator.BusinessLogic.Services;
+using AutoMapper;
+using FluentAssertions;
+using HardwareHero.Services.Shared.DTOs;
+using HardwareHero.Services.Shared.Exceptions;
+using HardwareHero.Services.Shared.Models.Aggregator;
+using HardwareHero.Services.Shared.Repositories.Contracts;
+using Moq;
+using System.Linq.Expressions;
+=======
+>>>>>>> 6b38c6b318f9e8ea2478d3e914f7535c735db017
 
 namespace Aggregator.Tests
 {
@@ -8,6 +20,302 @@ namespace Aggregator.Tests
 
         public ComponentServiceTests()
         {
+<<<<<<< HEAD
+        }
+
+        [Fact]
+        public async Task GetComponentsAsPageAsync_ReturnComponents()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var pageNumber = 1;
+            var pageSize = 10;
+            var specificationFilter = "{'key': 'value'}";
+            var searchString = "search";
+
+            var components = new List<Component> { new Component { Id = Guid.NewGuid() } };
+
+            mockComponentRepo.Setup(repo => repo.GetManyEntitiesAsync())
+                .ReturnsAsync(components.AsQueryable());
+            mockComponentRepo.Setup(repo => repo.GetPageAsync(It.IsAny<IQueryable<Component>>(), pageNumber, pageSize))
+                .ReturnsAsync(components);
+
+            // Act
+            var result = await service.GetComponentsAsPageAsync(pageNumber, pageSize, specificationFilter, searchString);
+
+            // Assert
+            result.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task GetComponentsAsPageAsync_ThrowPageOptionValidationException()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var pageNumber = 0;
+            var pageSize = 0;
+            var specificationFilter = "{'key': 'value'}";
+            var searchString = "search";
+
+            // Act & Assert
+            await Assert.ThrowsAsync<PageOptionsValidationException>(() =>
+                service.GetComponentsAsPageAsync(pageNumber, pageSize, specificationFilter, searchString));
+        }
+
+        [Fact]
+        public async Task GetComponentsAsPageAsync_ReturnEmptyList()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var pageNumber = 1;
+            var pageSize = 10;
+            var specificationFilter = "{'key': 'value'}";
+            var searchString = "search";
+
+            var components = new List<Component>();
+
+            mockComponentRepo.Setup(repo => repo.GetManyEntitiesAsync())
+                .ReturnsAsync(components.AsQueryable());
+
+            // Act
+            var result = await service.GetComponentsAsPageAsync(pageNumber, pageSize, specificationFilter, searchString);
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetComponentByIdAsync_ReturnComponent()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentId = Guid.NewGuid();
+            var component = new Component { Id = componentId };
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(component);
+
+            // Act
+            var result = await service.GetComponentByIdAsync(componentId);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task AddComponentAsync_ReturnNotEmptyValue()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentToAdd = new ComponentDto { Name = "ComponentName" };
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync((Component)null);
+            mockComponentRepo.Setup(repo => repo.CreateEntityAsync(It.IsAny<Component>()))
+                .ReturnsAsync(Guid.NewGuid());
+
+            // Act
+            var result = await service.AddComponentAsync(componentToAdd);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task AddComponentAsync_ThrowAlreadyExistException()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentToAdd = new ComponentDto { Name = "ComponentName" };
+            var existingComponent = new Component();
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(existingComponent);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<AlreadyExistException>(() =>
+                service.AddComponentAsync(componentToAdd));
+        }
+
+        [Fact]
+        public async Task RemoveComponentAsync_ThrowNotFoundException()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentId = Guid.NewGuid();
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync((Component)null);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                service.RemoveComponentAsync(componentId));
+        }
+
+        [Fact]
+        public async Task UpdateComponentAsync_ReturnTrue()
+        {
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentToUpdate = new ComponentDto { Id = Guid.NewGuid() };
+            var existingComponent = new Component { Id = componentToUpdate.Id };
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(existingComponent);
+            mockComponentRepo.Setup(repo => repo.UpdateEntityAsync(It.IsAny<Component>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await service.UpdateComponentAsync(componentToUpdate);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task UpdateComponentAsync_ThrowNotFoundException()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentToUpdate = new ComponentDto { Id = Guid.NewGuid() };
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync((Component)null);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                service.UpdateComponentAsync(componentToUpdate));
+        }
+
+        [Fact]
+        public async Task UpdateComponentAsync_ThrowAlreadyExistExtension()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentToUpdate = new ComponentDto { Id = Guid.NewGuid(), Name = "ComponentName" };
+            var existingComponent = new Component();
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(existingComponent);
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(existingComponent);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<AlreadyExistException>(() =>
+                service.UpdateComponentAsync(componentToUpdate));
+        }
+
+        [Fact]
+        public async Task GetComponentAvgMarkAsync_ReturnPositiveCount()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentId = Guid.NewGuid();
+            var component = new Component { Id = componentId };
+            var reviews = new List<ComponentReview>
+        {
+            new ComponentReview { ComponentId = componentId, Recommended = true },
+            new ComponentReview { ComponentId = componentId, Recommended = true },
+            new ComponentReview { ComponentId = componentId, Recommended = false }
+        };
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(component);
+            mockComponentReviewRepo.Setup(repo => repo.GetManyEntitiesAsync(It.IsAny<Expression<Func<ComponentReview, bool>>>()))
+                .ReturnsAsync(reviews.AsQueryable());
+
+            // Act
+            var result = await service.GetComponentAvgMarkAsync(componentId);
+
+            // Assert
+            result.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetComponentAvgMarkAsync_ReturnNotFoundException()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentId = Guid.NewGuid();
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync((Component)null);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                service.GetComponentAvgMarkAsync(componentId));
+        }
+
+        [Fact]
+        public async Task GetComponentAvgMarkAsync_ReturnZero()
+        {
+            // Arrange
+            var mockComponentRepo = new Mock<IPageRepositoryAsync<Component>>();
+            var mockComponentReviewRepo = new Mock<IPageRepositoryAsync<ComponentReview>>();
+            var mockMapper = new Mock<IMapper>();
+
+            var service = new ComponentService(mockComponentRepo.Object, mockComponentReviewRepo.Object, mockMapper.Object);
+            var componentId = Guid.NewGuid();
+            var component = new Component { Id = componentId };
+
+            mockComponentRepo.Setup(repo => repo.GetOneEntityAsync(It.IsAny<Expression<Func<Component, bool>>>()))
+                .ReturnsAsync(component);
+            mockComponentReviewRepo.Setup(repo => repo.GetManyEntitiesAsync(It.IsAny<Expression<Func<ComponentReview, bool>>>()))
+                .ReturnsAsync(Enumerable.Empty<ComponentReview>().AsQueryable());
+
+            // Act
+            var result = await service.GetComponentAvgMarkAsync(componentId);
+
+            // Assert
+            result.Should().Be(0);
+=======
             // Инициализация сервиса компонентов перед каждым тестом (если необходимо)
         }
 
@@ -191,6 +499,7 @@ namespace Aggregator.Tests
             // Act
 
             // Assert
+>>>>>>> 6b38c6b318f9e8ea2478d3e914f7535c735db017
         }
     }
 }

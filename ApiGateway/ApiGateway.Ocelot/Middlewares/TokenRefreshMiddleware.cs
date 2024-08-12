a4b04f8407ba5b3fdcfc1509162b2e479b76ba47
IlyaBuryn <ilya.buryn@gmail.com>
2024-08-12 15:14:47 +0300

    I just made ocelot a little more convenient.
    All requests are separated into files and middleware for authentication and cookies is added.

---
+++ ApiGateway/ApiGateway.Ocelot/Middlewares/TokenRefreshMiddleware.cs
@@ -0,0 +1,103 @@
+﻿using EventStream.EventHandling;
+using HardwareHero.Shared.Exceptions;
+using HardwareHero.Shared.Responses;
+using KafkaEventStream.BackgroundServices;
+using KafkaEventStream.Topics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+
+namespace ApiGateway.Ocelot.Middlewares
+{
+    public class TokenRefreshMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+        private readonly IMessageProducer _messageProducer;
+        private readonly IMessageConsumer _messageConsumer;
+        private readonly List<(string PathTemplate, string HttpMethod)> _excludedRoutes;
+
+        public TokenRefreshMiddleware(
+            RequestDelegate next,
+            IConfiguration configuration,
+            IMessageProducer messageProducer,
+            IMessageConsumer messageConsumer)
+        {
+            _next = next;
+            _configuration = configuration;
+            _messageProducer = messageProducer;
+            _messageConsumer = messageConsumer;
+            _excludedRoutes = new List<(string, string)>
+            {
+                ("/identity/account/sign-up", "POST"),
+                ("/identity/account/sign-in", "POST"),
+                ("/aggregator/components/page", "POST"),
+            };
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Skip if auth isn't required
+            var requestPath = context.Request.Path.Value;
+            var requestMethod = context.Request.Method.ToUpper();
+
+            if (_excludedRoutes.Any(route =>
+                string.Equals(route.PathTemplate, requestPath, StringComparison.OrdinalIgnoreCase)
+                && string.Equals(route.HttpMethod, requestMethod, StringComparison.OrdinalIgnoreCase)))
+            {
+                await _next(context);
+                return;
+            }
+
+            var jwtCookieKey = _configuration.GetSection("JwtConfig:JwtCookieKey").Value ?? string.Empty;
+            var refreshCookieKey = _configuration.GetSection("JwtConfig:RefreshCookieKey").Value ?? string.Empty;
+
+            // Get tokens from cookies
+            var accessToken = string.Empty;
+            var refreshToken = string.Empty;
+
+
+            accessToken = context.Request.Cookies[jwtCookieKey]?.Replace("Bearer ", "");
+            refreshToken = context.Request.Cookies[refreshCookieKey];
+            if (string.IsNullOrEmpty(accessToken) || string.IsNullOrEmpty(refreshToken)) 
+            { 
+                throw new AuthenticationException();
+            }
+
+            // check if token is Expired
+            if (IsTokenExpired(accessToken))
+            {
+                // Send message to Identity API
+                var identityResponseJson = await RequestService.CallAndWaitServiceAsync(
+                    new IdentityTopics(), $"refresh-token/{accessToken}/{refreshToken}", 
+                    _messageProducer, _messageConsumer);
+
+                var identityResponse = JsonSerializer.Deserialize<AuthenticationResponse>(identityResponseJson);
+
+                if (identityResponse == null || !identityResponse.IsSuccessful)
+                {
+                    throw new AuthenticationException(identityResponse.Errors.First());
+                }
+
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict
+                };
+
+                context.Response.Cookies.Append(jwtCookieKey, identityResponse.AccessToken, cookieOptions);
+                context.Response.Cookies.Append(refreshCookieKey, identityResponse.RefreshToken, cookieOptions);
+            }
+
+            await _next(context);
+        }
+
+        private bool IsTokenExpired(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+
+            return jwtToken.ValidTo < DateTime.UtcNow;
+        }
+    }
+}
--- ApiGateway/ApiGateway.Ocelot/Program.cs
+++ ApiGateway/ApiGateway.Ocelot/Program.cs
@@ -1,16 +1,32 @@
 using Ocelot.DependencyInjection;
+using HardwareHero.Shared.Extensions;
 using Ocelot.Middleware;
+using ApiGateway.Ocelot.Extensions;
+using KafkaEventStream.Topics;
+using KafkaEventStream.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureSecretsFile();
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-
+builder.ConfigureOcelotFile();
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.ConfigureKafkaRequestsBackgroundWorker<IdentityTopics>();
+builder.Services.ConfigureCORSPolicy();
 
 var app = builder.Build();
 
+app.UseCommonCustomMiddlewares();
+
+app.UseCors("default");
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseTokenRefreshMiddleware();
+
+app.UseHttpsRedirection();
+
 await app.UseOcelot();
+
 
 app.Run();

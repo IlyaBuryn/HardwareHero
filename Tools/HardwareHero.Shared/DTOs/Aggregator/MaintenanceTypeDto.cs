dc59d3bac26bc8ec7407ef5b6f4dda48bd474fc9
IlyaBuryn <ilya.buryn@gmail.com>
2024-11-05 16:23:45 +0300

    Got rid of `Maintenance`.
    Because it's not being used, yet, and there will be a separate service dedicated to that.

--- Tools/HardwareHero.Shared/DTOs/Aggregator/MaintenanceTypeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HardwareHero.Shared.DTOs.Aggregator
-{
-    public class MaintenanceTypeDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Tools/HardwareHero.Shared/Models/Aggregator/MaintenanceType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HardwareHero.Shared.Models.Aggregator
-{
-    public class MaintenanceType : BaseEntity
-    {
-        public string Name { get; set; }
-    }
-}

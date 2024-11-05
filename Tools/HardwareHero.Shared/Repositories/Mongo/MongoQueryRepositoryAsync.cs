55e0d7320151fd859dc02ae4e04116e083757aef
IlyaBuryn <ilya.buryn@gmail.com>
2024-11-05 16:13:18 +0300

    MongoDb Repositories.
    For some reason they weren't there.

---
+++ Tools/HardwareHero.Shared/Repositories/Mongo/MongoQueryRepositoryAsync.cs
@@ -0,0 +1,26 @@
+﻿using HardwareHero.Filter.Operations;
+using HardwareHero.Shared.Repositories.Answers;
+using HardwareHero.Shared.Repositories.Contracts;
+using MongoDB.Driver;
+
+namespace HardwareHero.Shared.Repositories.Mongo
+{
+    public class MongoQueryRepositoryAsync<T> : MongoBaseRepositoryAsync<T>, IQueryRepositoryAsync<T>
+        where T : BaseEntity
+    {
+        public MongoQueryRepositoryAsync(IMongoDatabase database, string collectionName)
+            : base(database, collectionName) { }
+
+        public Task<DataAnswer<IQueryable<T>>> FindAsync(Func<T, bool> predicate, params Expression<Func<T, object>>[] includeProperties)
+        {
+            // TODO: --
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedDataAnswer<T>> FindPagedAsync(Func<T, bool>? predicate, IPaginable filter, params Expression<Func<T, object>>[] includeProperties)
+        {
+            // TODO: --
+            throw new NotImplementedException();
+        }
+    }
+}

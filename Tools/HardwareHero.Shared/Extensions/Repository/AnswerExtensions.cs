af123406556baabdd3e7af19b87293483fc4a592
IlyaBuryn <ilya.buryn@gmail.com>
2024-11-13 22:03:41 +0300

    Changes in extensions.

---
+++ Tools/HardwareHero.Shared/Extensions/Repository/AnswerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using HardwareHero.Shared.Repositories.Answers;
+
+namespace HardwareHero.Shared.Extensions.Repository
+{
+    public static class AnswerExtensions
+    {
+        /// <summary>
+        /// Check exceptions and `IsSuccess` status.
+        /// Doesn't affect the value itself
+        /// </summary>
+        public static void DataAnswerCheck<T>(
+            this Answer<T> answer) where T : class
+        {
+            if (answer.Errors.Any())
+            {
+                throw answer.Errors.First();
+            }
+
+            if (answer.IsSuccess)
+            {
+                throw new InvalidOperationException("Find 0 errors, but answer is negative!");
+            };
+        }
+    }
+}

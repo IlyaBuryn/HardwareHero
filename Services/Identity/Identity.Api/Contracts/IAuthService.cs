a0d95a3047c09f4ef852e2957a9f6f8d5f28e22f
IlyaBuryn <ilya.buryn@gmail.com>
2024-11-13 22:13:48 +0300

    Updated Identity.Api.
    Fix for previous commits.

--- Services/Identity/Identity.Api/Contracts/IAuthService.cs
+++ Services/Identity/Identity.Api/Contracts/IAuthService.cs
@@ -1,11 +1,16 @@
-﻿using static Identity.Api.Records.RequestModels;
+﻿using Identity.Shared.Domain;
+using Identity.Shared.Requests;
+using Identity.Shared.Responses;
+using static Identity.Shared.Requests.IdentityRequestRecords;
 
 namespace Identity.Api.Contracts
 {
     public interface IAuthService
     {
-        Task<ApplicationUser> SignInAsync(SignInRequestModel model);
-        Task<ApplicationUser> SignUpAsync(SignUpRequestModel model);
+        Task<ApplicationUser> SignInAsync(SignInRequest model);
+        Task<ApplicationUser> SignUpAsync(SignUpRequest model);
+
+        Task<AuthenticationResponse> UpdatePasswordAsync(UserPasswordChangeRequest model);
         Task<AuthenticationResponse> RefreshTokenAsync(TokenRequest tokenRequest);
         Task<AuthenticationResponse?> VerifyTokenAsync(TokenRequest tokenRequest);
         Task<AuthenticationResponse> GenerateJwtTokenAsync(ApplicationUser user);
--- Services/Identity/Identity.Api/Controllers/AuthenticateController.cs
+++ Services/Identity/Identity.Api/Controllers/AuthenticateController.cs
@@ -1,10 +1,13 @@
 ﻿using EventStream.EventHandling;
 using Identity.Api.Contracts;
+using Identity.Shared.Requests;
+using Identity.Shared.Responses;
 using KafkaEventStream.BackgroundServices;
-using static Identity.Api.Records.RequestModels;
+using static Identity.Shared.Requests.IdentityRequestRecords;
 
 namespace Identity.Api.Controllers
 {
+    [Authorize(AuthenticationSchemes = "Bearer")]
     [Produces("application/json")]
     [Route("api/identity/account")]
     [ApiController]
@@ -29,10 +32,11 @@ namespace Identity.Api.Controllers
 
         [HttpPost("sign-up")]
         [AllowAnonymous]
-        public async Task<IActionResult> SignUpUserAsync([FromBody] SignUpRequestModel model)
+        public async Task<IActionResult> SignUpUserAsync([FromBody] SignUpRequest model)
         {
             var user = await _authService.SignUpAsync(model);
             var result = await _authService.GenerateJwtTokenAsync(user);
+            result.UserId = user.Id;
 
             if (result.IsSuccessful)
             {
@@ -40,17 +44,29 @@ namespace Identity.Api.Controllers
                     new MailTopics(), $"mail/welcome/{user.Email}", _messageProducer, _messageConsumer);
             }
             
-            AppendCookies(result);
+            AppendCookies(result, model.StayIn);
 
             return Created(nameof(SignUpUserAsync), result);
         }
 
         [HttpPost("sign-in")]
         [AllowAnonymous]
-        public async Task<IActionResult> SignInUserAsync([FromBody] SignInRequestModel model)
+        public async Task<IActionResult> SignInUserAsync([FromBody] SignInRequest model)
         {
             var user = await _authService.SignInAsync(model);
             var result = await _authService.GenerateJwtTokenAsync(user);
+            result.UserId = user.Id;
+
+            AppendCookies(result, model.StayIn);
+
+            return Ok(result);
+        }
+
+        [HttpPut("password")]
+        [Authorize(Roles = Roles.User)]
+        public async Task<IActionResult> UpdatePasswordAsync([FromBody] UserPasswordChangeRequest model)
+        {
+            var result = await _authService.UpdatePasswordAsync(model);
 
             AppendCookies(result);
 
@@ -68,14 +84,21 @@ namespace Identity.Api.Controllers
             return Ok(result);
         }
 
-        private void AppendCookies(AuthenticationResponse model)
+        private void AppendCookies(AuthenticationResponse model, bool userStayIn = true)
         {
             var cookieOptions = new CookieOptions
             {
                 HttpOnly = false,
-                Secure = true,
-                SameSite = SameSiteMode.Strict,
+                Secure = false,
+                SameSite = SameSiteMode.Lax, // TODO:
+                Domain = "localhost", // TODO:
+                Path = "/", // TODO:
             };
+
+            if (userStayIn)
+            {
+                cookieOptions.Expires = DateTime.Now + TimeSpan.FromDays(30);
+            }
 
             var jwtCookieKey = _configuration.GetSection("JwtConfig:JwtCookieKey").Value;
             var refreshCookieKey = _configuration.GetSection("JwtConfig:RefreshCookieKey").Value;

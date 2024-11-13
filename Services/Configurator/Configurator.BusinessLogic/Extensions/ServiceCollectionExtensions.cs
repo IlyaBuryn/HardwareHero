c3943a93d4bc9d9d2aa411c4eb7f996c96d6e071
IlyaBuryn <ilya.buryn@gmail.com>
2024-11-13 23:46:48 +0300

    Update ServiceCollectionExtensions.cs

--- Services/Configurator/Configurator.BusinessLogic/Extensions/ServiceCollectionExtensions.cs
+++ Services/Configurator/Configurator.BusinessLogic/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
+
 namespace Configurator.BusinessLogic.Extensions
 {
     public static class ServiceCollectionExtensions

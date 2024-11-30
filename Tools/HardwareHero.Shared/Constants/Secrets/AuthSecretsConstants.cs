2277a7cb67ba28bd02a3e3bffbf7327c037b873b
IlyaBuryn <ilya.buryn@gmail.com>
2024-11-30 22:19:09 +0300

    Moved the extension that connects the secrets file.

---
+++ Tools/HardwareHero.Shared/Constants/Secrets/AuthSecretsConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace HardwareHero.Shared.Constants.Secrets
+{
+    public static class AuthSecretsConstants
+    {
+        public const string FilePath = "/src/.credentials/sharedsettings.json";
+    }
+}
---
+++ Tools/HardwareHero.Shared/Extensions/Secrets/AuthSecretsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using HardwareHero.Shared.Constants.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace HardwareHero.Shared.Extensions.Secrets
+{
+    public static class AuthSecretsExtensions
+    {
+        public static IHostBuilder ConfigureAuthenticationSecretsFiles(
+            this IHostBuilder host)
+        {
+            host.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile(
+                    AuthSecretsConstants.FilePath, 
+                    optional: true, 
+                    reloadOnChange: true);
+            });
+
+            return host;
+        }
+    }
+}
